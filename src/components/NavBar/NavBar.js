7021b01a2b5242ca5c45c6db2586d9a9d5c17e68
Kushichka <kushichka@gmail.com>
2022-10-18 17:48:50 +0200

    initial commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import style from './NavBar.module.scss';
+
+const NavBar = () => {
+    return (
+        <div className={style.navBar}>
+            world
+        </div>
+    );
+}
+ 
+export default NavBar;
